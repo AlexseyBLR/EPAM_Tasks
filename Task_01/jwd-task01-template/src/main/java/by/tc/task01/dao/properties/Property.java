f5dddeb2151d033b3f0c8b746b4dd2adb45a5dab
OSmol <Olga_Smolyakova@outlook.com>
2018-01-30 13:17:10 +0300

    Update Property.java

--- Task_01/jwd-task01-template/src/main/java/by/tc/task01/dao/properties/Property.java
+++ Task_01/jwd-task01-template/src/main/java/by/tc/task01/dao/properties/Property.java
@@ -5,5 +5,5 @@ import java.io.File;
 public final class Property {
 
     public static final String path = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "appliances_db.txt";
-
+// это в каком такой src ты фалы кладешь???? как же ты приложение вне ide собрался запускать?
 }
