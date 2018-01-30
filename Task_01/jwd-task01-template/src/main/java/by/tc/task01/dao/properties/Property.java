169f83391ac9e518170e5d40862cab3b044696f9
OSmol <Olga_Smolyakova@outlook.com>
2018-01-30 13:17:37 +0300

    Update Property.java

--- Task_01/jwd-task01-template/src/main/java/by/tc/task01/dao/properties/Property.java
+++ Task_01/jwd-task01-template/src/main/java/by/tc/task01/dao/properties/Property.java
@@ -3,7 +3,7 @@ package by.tc.task01.dao.properties;
 import java.io.File;
 
 public final class Property {
-
+// конструктор у этого класса должен быть private
     public static final String path = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "appliances_db.txt";
 // это в каком такой src ты фалы кладешь???? как же ты приложение вне ide собрался запускать?
 }
